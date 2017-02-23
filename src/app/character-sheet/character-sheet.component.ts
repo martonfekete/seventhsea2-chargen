2c4b036027b62312d1ee408d9a7e5967554c6546
Marton Fekete <mafekete89@gmail.com>
2017-02-23 13:25:08 +0100

    fix printing/saving option

--- src/app/character-sheet/character-sheet.component.ts
+++ src/app/character-sheet/character-sheet.component.ts
@@ -167,9 +167,325 @@ export class CharacterSheetComponent implements OnInit {
 		this.refresh.emit('reset');
 	}
 
+	print(): void {
+		let printContents, popupWin;
+	    printContents = document.getElementById('char-sheet').innerHTML;
+	    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+	    popupWin.document.open();
+	    popupWin.document.write(`
+	      <html moznomarginboxes mozdisallowselectionprint>
+	        <head>
+	          <title>Character Sheet</title>
+	            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	          <style media="all">
+				body {
+					margin: 0;
+				}
+				#wrapper {
+					background-color: rgba(255,255,255,.95);
+					position: fixed;
+					width: 100%;
+					height: 100%;
+					z-index: -1;
+				}
+				app-root {
+					padding:8px;
+				}
+				body, input, md-select {
+					font-size:10pt;
+				}
+				h1 {
+					font-size:1.5em;
+				}
+				h2, .story-card h2 {
+					font-size:1.3em;
+				}
+				h3 {
+					font-size:1.15em;
+					font-weight: normal;
+					padding-left: .5em;
+				}
+				md-select {
+					margin-top:22px;
+				}
+				.margin-bottom {
+					margin-bottom: 1em!important;
+				}
+				.margin-left {
+					margin-left: 1em!important;
+				}
+				.margin-top {
+					margin-top: 1em!important;
+				}
+				.margin-right {
+					margin-right: 1em!important;
+				}
+				.no-margin {
+					margin: 0 !important;
+				}
+				.padding {
+					padding: 1em !important;
+				}
+				.strong-text {
+					font-weight: bold;
+				}
+				.md-caption .md-list-item {
+					height: auto;
+				}
+				.md-caption p, .skill-caption {
+					font-size: smaller;
+					color: grey;
+				}
+				.md-caption p {
+					margin-left: 1em;
+				}
+				.select-wide, md-select {
+					width:180px;
+				}
+				.bg-grey {
+					background: rgba(155,155,155,.5);
+				}
+				.bg-darkgrey {
+					background: rgba(20,20,20,.5) !important;
+				}
+				.text-grey {
+					color: grey;
+				}
+				.bg-shade {
+					background: rgba(178, 34, 34,.2);
+				}
+				md-input-container {
+					width: 100%;
+					margin-top:1em;
+				}
+
+				.char-box {
+					width:calc(50% - 1em);
+					margin:.4em;
+					padding:1em;
+					box-sizing: border-box;
+				}
+
+				.char-box.small-box {
+					min-width: 350px;
+					max-width: 400px;
+				}
+
+				.char-box h2, md-grid-tile h2 {
+					padding:.2em 1em .1em;
+					color: white;
+					background: firebrick;
+					text-transform: uppercase;
+					font-weight: normal;
+				}
+				.inline {
+					display: inline-block;
+					vertical-align: top;
+				}
+				.inline-multi {
+					float:left;
+				}
+
+				md-card-content ol {
+					margin-left: 0;
+					padding-left: 1em;
+				}
+				.pointer {
+					cursor: pointer;
+				}
+				.text-red {
+					color: firebrick;
+				}
+
+				md-grid-tile figure {
+					justify-content: flex-start !important;
+				}
+				.inline-icon input {
+					width: 90% !important;
+					float: left;
+				}
+				.inline-icon md-icon {
+					font-size:1.25em;
+					color: firebrick;
+					float: right;
+				}
+				.linkbutton {
+					color: inherit;
+				}
+
+				.story-card {
+					padding: 0;
+				}
+				.story-card md-card-title {
+					font-size: inherit;
+				}
+				.story-card md-card-subtitle {
+					border-bottom: thin solid firebrick;
+					padding: 0 0 .5em 1em;
+					margin: 0;
+					color: inherit;
+				}
+				.story-card table {
+					width:100%;
+					font-size: small;
+					text-align: center;
+					border-bottom: thin solid firebrick;
+				}
+				.story-card p {
+					margin-left: 1em;
+					margin-bottom: 10px;
+					width:50px;
+					border-bottom: thin dashed firebrick;
+				}
+				.story-card md-card-content ol {
+					font-size: small;
+					padding-bottom: 1em;
+					margin-left: 1em;
+				}
+				.story-card md-card-content ol li {
+					margin-left: 0;
+					padding-left: 5px;
+				}
+				.text-truncate {
+					text-overflow: ellipsis;
+				    white-space: nowrap;
+				    overflow: hidden;
+				}
+				.indented h3 {
+					font-size: 1em;
+					font-weight: bold;
+					padding-bottom: 5px;
+					margin-bottom: 0;
+					border-bottom: thin solid firebrick;
+				}
+				.indented p {
+					padding-left: 1em;
+				}
+				.align-center {
+					text-align: center;
+				}
+				.inline-number {
+					display: inline-block;
+					width: 30px;
+					margin-top: 5px;
+				}
+				md-slide-toggle.inline {
+					margin: 0 0 0 30px;
+				}
+				p.inline {
+					margin: 4px 30px 0 0;
+					vertical-align: middle;
+				}
+				.rank-list {
+					margin-top: 0;
+					margin-left:0;
+					padding-left:0;
+					list-style-type: none;
+				}
+				.rank-list li {
+					padding: 5px 10px; 
+				}
+				.inline-check {
+					position: relative;
+					top:20px;
+					margin: 0 2em 0 1em; 
+				}
+				.italic {
+					font-style: italic;
+				}
+				.char-box {
+					font-family: sans-serif;
+				}
+				.char-box.small {
+					max-width: calc(100% / 4 - 15px);
+				}
+				.char-box h2 {
+					margin-bottom: 0;
+					text-align: center;
+					font-size: 1.15em;
+				}
+				.char-box h4 {
+					margin-bottom: 0;
+				}
+				.rank-name {
+					display: inline-block;
+					width:75px;
+				}
+				md-icon:not(.regular) {
+					font-size: 16px;
+					height: 16px;
+					width: 16px;
+					position: relative;
+					top: 2px;
+				}
+				.align {
+					position: relative;
+				    top: -1px;
+				}
+				.sep {
+					width:10px;
+				}
+				.char-box-block {
+					margin-top:10px;
+				}
+
+				.char-box-block p:not(.md-caption) {
+					padding: 0;
+					margin: 0 5px 5px 10px;
+				}
+				p.md-caption {
+					margin: 0;
+					padding: 0;
+					font-size: smaller;
+					color: grey;
+					margin-left: 15px;
+				}
+				.story-card {
+					box-shadow: none;
+				}
+				.story-card md-card-title {
+					margin-bottom: 10px;
+				}
+				.story-card md-card-subtitle, .story-card table td, .story-card ol li {
+					font-size: smaller;
+				}
+				table.wounds-table {
+					table-layout: fixed;
+					margin: auto;
+				}
+				table.wounds-table .strong-text {
+					position: relative;
+					top: 4px;
+				}
+				td.with-explanation {
+					width:115px;
+				}
+				table.wounds-table .sep {
+					font-size: smaller;
+					margin-left: 10px;
+					font-style: italic;
+					color: grey;
+				}
+				table.wounds-table .sep md-icon {
+					top: 4px;
+				}
+				.dramatic-icon {
+					font-size: x-large;
+				    top: 5px;
+				    color: grey;
+				}
+				@page {
+					size: auto; margin: 0; }
+				}
+	          </style>
+	        </head>
+	    <body onload="window.print();window.close()">` + printContents + `</body>
+	      </html>`
+	    );
+	    popupWin.document.close();
+	}
+
 	printSheet(): any {
-    //var toPrint = document.getElementById('printThis');
-    //html2canvas(toPrint, {
     html2canvas(document.body, {
       onrendered: function(canvas) {
         //document.body.appendChild(canvas);
